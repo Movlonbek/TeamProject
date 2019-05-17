28ca60cb3f695ec81c08dd07ee38a0c4511dd1ae
Mike G <wormike96@gmail.com>
2019-05-17 06:08:21 -0400

    Netflix Test added

--- Generic/src/main/java/CommonAPI.java
+++ Generic/src/main/java/CommonAPI.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
-import sun.rmi.rmic.iiop.Generator;
+
 
 import java.io.File;
 import java.io.IOException;
