cd77381f1924b46a074223dd1c94fde562d3a21d
ahmedkawsar21 <49250362+ahmedkawsar21@users.noreply.github.com>
2019-05-19 21:35:50 -0400

    Add files via upload

---
+++ Amazon/src/main/regression/Homepage.java
@@ -0,0 +1,50 @@
+package regression;
+
+import base.CommonAPI;
+import org.openqa.selenium.WebElement;
+
+public class Homepage extends CommonAPI {
+    public void clickAccount() {
+
+    }
+
+    public void getEmailOrNumber() {
+
+    }
+
+    public void getSignInPassword() {
+    }
+
+    public void SignIn(String s, String s1) {
+    }
+
+    public void signInButton() {
+
+    }
+
+    public void CreateYourAmazonAccount() {
+        clickAccount();
+
+
+    }
+
+
+    public void clickOrders() {
+    }
+
+    public void clickTryPrime() {
+    }
+
+    public void clickCart() {
+    }
+
+    public void searchBox() {
+
+    }
+
+    public void ClickShopDealsOfTheDay() {
+
+
+    }
+
+}
