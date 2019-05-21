fd65bd2aa688c92efd5d9a03eb46b5379741bddf
Tazz Alam <Tazzalam@gmail.com>
2019-05-21 18:43:33 -0400

    couple of testacese more

---
+++ Coinbase/src/main/java/HomePage.java
@@ -0,0 +1,91 @@
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends CommonAPI {
+
+    //Storing all the WebElements of the Homepage for the test
+
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/div/div[1]")
+    public static WebElement signInButton;
+
+    @FindBy(xpath="//*[@id=\"root\"]/div/div/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/div/div[2]/button" )
+    public static WebElement getStarted;
+
+    @FindBy (xpath ="//*[@id=\"root\"]/div/div/div[1]/div[1]/div[2]/div[2]/div/div/nav/div[1]/div/span" )
+    public static WebElement products;
+
+    @FindBy(xpath ="//*[@id=\"root\"]/div/div/div[1]/div[1]/div[2]/div[2]/div/div/nav/div[2]/a")
+    public static WebElement prices;
+
+    @FindBy(xpath = "//*[@id=\"main\"]/div/form/input")
+    public static WebElement email;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[9]/div/div/div[2]/a[2]")
+    public static WebElement commerce;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[9]/div/div/div[2]/a[3]")
+    public static WebElement custody;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[9]/div/div/div[2]/a[4]")
+    public static WebElement earn;
+
+    @FindBy(xpath="//*[@id=\"root\"]/div/div/div[9]/div/div/div[2]/a[5]")
+    public static WebElement pro;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[9]/div/div/div[2]/a[6]")
+    public static WebElement USDcoin;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[9]/div/div/div[2]/a[7]")
+    public static WebElement wallet;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[9]/div/div/div[3]/a[1]")
+    public static WebElement buyBitcoin;
+
+
+
+    public void signInClick(){
+        signInButton.click();
+    }
+    public void getStartedClick(){
+        getStarted.click();
+    }
+    public void productsClick(){
+        products.click();
+    }
+    public void pricesClick(){
+        prices.click();
+    }
+    public void emailInput(String email1){
+        email.sendKeys(email1);
+    }
+
+    public void commerceClick(){
+        commerce.click();
+    }
+
+    public void custodyClick(){
+        custody.click();
+    }
+
+    public void earnClick(){
+        earn.click();
+    }
+
+    public void proClick(){
+        pro.click();
+    }
+
+    public void USDcoinClick(){
+        USDcoin.click();
+    }
+
+    public void walletClick(){
+        wallet.click();
+    }
+
+    public void buyBitcoinClick(){
+        buyBitcoin.click();
+    }
+
+}
--- Coinbase/src/test/java/TestHomePage.java
+++ Coinbase/src/test/java/TestHomePage.java
@@ -1,43 +1,90 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestHomePage extends CommonAPI {
-    //Storing all the WebElements of the Homepage for the test
-    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[1]/div[1]/div[2]/div[2]/div/div/div[1]/a]")
-    public static WebElement homepage;
+    public HomePage homepage;
 
-    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/div/div[1]")
-    public static WebElement signInButton;
-
-    @FindBy(xpath="//*[@id=\"root\"]/div/div/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/div/div[2]/button" )
-    public static WebElement getStarted;
-
-    @FindBy (xpath ="//*[@id=\"root\"]/div/div/div[1]/div[1]/div[2]/div[2]/div/div/nav/div[1]/div/span" )
-    public static WebElement products;
-
-    @FindBy(xpath ="//*[@id=\"root\"]/div/div/div[1]/div[1]/div[2]/div[2]/div/div/nav/div[2]/a")
-    public static WebElement prices;
-
-
-
-
-
-    @Test
-
-public void homepageTestCase(){
-    homepage.click();
-
+    @BeforeMethod
+    public void init() {
+        homepage = PageFactory.initElements(driver, HomePage.class);
     }
 
-    public void signInButtonTestCase(){
-        signInButton.click();
 
+    public void signInTestCase() {
+        homepage.signInClick();
+        sleepFor(4);
     }
+
     public void getStartedTestCase(){
-        getStarted.click();
+        homepage.getStartedClick();
+        sleepFor(4);
+    }
+
+    public void productTestCase(){
+        homepage.productsClick();
+        sleepFor(4);
+    }
+
+    public void pricesTestCase(){
+        homepage.pricesClick();
+        sleepFor(4);
+    }
+    @Test
+    public void emailTestCase(){
+        homepage.emailInput("john@gmail.com");
+        sleepFor(4);
+
+    }
+
+    public void commerceTestCase(){
+        homepage.commerceClick();
+        sleepFor(4);
+    }
+
+    public void custodyTestCase(){
+        homepage.custodyClick();
+        sleepFor(4);
+    }
+
+    public void earnTestCase(){
+        homepage.earnClick();
+        sleepFor(4);
+    }
+
+    public void proTestCase(){
+        homepage.pricesClick();
+        sleepFor(4);
+    }
+
+    public void USDcoinTestCase(){
+        homepage.USDcoinClick();
+        sleepFor(4);
+    }
+
+    public void walletTestCase(){
+        homepage.walletClick();
+        sleepFor(4);
+    }
+
+    public void buyBitcoinTestCase(){
+        homepage.buyBitcoinClick();
+        sleepFor(4);
     }
 
 
 }
+
+
+
+
+
+
+
+
+
+
